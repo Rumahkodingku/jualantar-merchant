97852b6aeca150b7b88e58b5267def3d665ccbfa
xRiot45 <tomasalberto527@gmail.com>
2026-09-15 09:11:24 +0700

    feat: add allowed host on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(({ mode }) => {
                     changeOrigin: true,
                 },
             },
+            allowedHosts: ["merchant.jualantar.test"],
         },
     }
 })
